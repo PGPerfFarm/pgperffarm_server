ec1b3c161cb268497ef10ab26a4379d91c8689cb
Hongyuan Ma <CS_MaleicAcid@163.com>
2018-05-28 22:08:19 +0800

    add case sensitive in .gitignore

---
+++ front-end/src/util/basic-table/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
+import './index.css';
+
+// general basic table
+class BasicTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFirstLoading: true
+        }
+    }
+
+
+    render() {
+        const data = [{
+            name: 'Tanner Linsley',
+            age: 26,
+            friend: {
+                name: 'Jason Maurer',
+                age: 23,
+            }
+        }]
+
+        const columns = [{
+                Header: 'Name',
+                accessor: 'name' // String-based value accessors!
+            }, {
+                Header: 'Age',
+                accessor: 'age',
+                Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+            }, {
+                id: 'friendName', // Required because our accessor is not a string
+                Header: 'Friend Name',
+                accessor: d => d.friend.name // Custom value accessors!
+            }, {
+                Header: props => <span>Friend Age</span>, // Custom header components!
+                accessor: 'friend.age'
+            }]
+        return (
+            <ReactTable className="ReactTable" data = {data} columns = {columns}/>
+        );
+
+    }
+}
+
+export default BasicTable;
