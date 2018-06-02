861d7488653e114c5a3903073ec1b0879ccc2fc6
MaleicAcid <CS_MaleicAcid@163\.com>
2018-06-02 17:29:23 +0800

    add TestRecord serializer

--- web/apps/test_records/serializer.py
+++ web/apps/test_records/serializer.py
@@ -1,3 +1,21 @@
 from rest_framework import serializers
+from .models import TestRecord
+class TestRecordSerializer(serializers.ModelSerializer):
 
-class TestRecordSerializer(serializers):
+    '''
+    use ModelSerializer
+    '''
+    class Meta:
+        model = TestRecord
+        fields = "__all__"
+
+    # test_machine_id = serializers.ForeignKey(UserMachine, verbose_name="test owner",
+    #                                     help_text="person who add this test item")
+    # pg_info = serializers.ForeignKey(PGInfo, verbose_name="pg info", help_text="pg info")
+    # meta_info = serializers.ForeignKey(MetaInfo, verbose_name="meta info", help_text="meta info")
+
+    # def create(self, validated_data):
+    #     """
+    #     Create and return a new `Snippet` instance, given the validated data.
+    #     """
+    #     return TestRecord.objects.create(**validated_data)
--- web/apps/test_records/views.py
+++ web/apps/test_records/views.py
@@ -2,17 +2,25 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .serializer import TestRecordSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 from .models import TestRecord
 # Create your views here.
-class SnippetList(APIView):
+class TestRecordListView(APIView):
     """
-    List all snippets, or create a new snippet.
+    List all records, or create a new record.
     """
     def get(self, request, format=None):
-        testRecord = TestRecord.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+        testRecords = TestRecord.objects.all()
+        records_serializer = TestRecordSerializer(testRecords, many=True)
+        return Response(records_serializer.data)
+
+    def post(self, request, format=None):
+        serializer = TestRecordSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
