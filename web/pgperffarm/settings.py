2f11c8f147578231cc700df7b138a3bc06dd5a32
Hongyuan Ma <CS_MaleicAcid@163.com>
2018-07-02 23:59:22 +0800

    add SessionAuthentication

--- web/pgperffarm/settings.py
+++ web/pgperffarm/settings.py
@@ -136,6 +136,11 @@ REST_FRAMEWORK = {
     #     'rest_framework.authentication.SessionAuthentication',
     # ),
     # 'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20
 }
