453a9609906437aff163cca6ad1189ac5651aeb3
Ila <ilaria.battiston@gmail.com>
2019-08-19 12:31:45 +0200

    changed wsgi

--- rest_api/rest_api/wsgi.py
+++ rest_api/rest_api/wsgi.py
@@ -12,6 +12,6 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rest_api.settings")
 
 application = get_wsgi_application()
