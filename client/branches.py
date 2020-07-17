976b3aeba6d8900d10b840299bf8ee1b1fe3bc82
Ila <ilaria.battiston@gmail.com>
2020-07-17 15:27:53 +0200

    updated branches

--- client/branches.py
+++ client/branches.py
@@ -5,5 +5,13 @@ branches = [
     {
         'branch_name': 'REL_13_STABLE',
     },
-   
+    {
+        'branch_name': 'REL_12_STABLE',
+    },
+    {
+        'branch_name': 'REL_11_STABLE',
+    },
+    {
+        'branch_name': 'REL_10_STABLE',
+    },
 ]
