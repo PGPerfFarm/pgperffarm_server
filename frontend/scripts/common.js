39d03098986f0dec58aca139cb0b7377bba8cae2
rulyox <email@rulyox.com>
2021-07-01 22:47:58 +0900

    Added escape function on unreliable values

---
+++ frontend/scripts/common.js
@@ -0,0 +1,16 @@
+const escapeHtml = (string) => {
+
+    var entityMap = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;',
+        '/': '&#x2F;',
+        '`': '&#x60;',
+        '=': '&#x3D;'
+    };
+
+    return String(string).replace(/[&<>"'`=\/]/g, (s) => entityMap[s]);
+
+};
