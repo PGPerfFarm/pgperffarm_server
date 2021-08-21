c92486248d052f57527f96b5bf8a34486a9d2c14
rulyox <email@rulyox.com>
2021-08-21 19:42:03 +0900

    Removed vue frontend

--- pgperffarm-front-end/dist/static/js/0.a6d9e565b5a98b2d5f38.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0],{FeBl:function(e,n){var t=e.exports={version:"2.6.11"};"number"==typeof __e&&(__e=t)},mvHQ:function(e,n,t){e.exports={default:t("qkKv"),__esModule:!0}},qkKv:function(e,n,t){var r=t("FeBl"),i=r.JSON||(r.JSON={stringify:JSON.stringify});e.exports=function(e){return i.stringify.apply(i,arguments)}}});
-//# sourceMappingURL=0.a6d9e565b5a98b2d5f38.js.map
