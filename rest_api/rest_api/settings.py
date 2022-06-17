f02992fc327fceccf9260999d587ac3f5eb973f1
Yedil·Serzhan <yedilserzhan@gmail.com>
2022-06-17 14:40:13 +0200

    changed settings.py for static files and templates

--- rest_api/rest_api/settings.py
+++ rest_api/rest_api/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'rest_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,6 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # limit body size to 10MB
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
