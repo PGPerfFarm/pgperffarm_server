23c2c9f4a9efd5a9ba1cba3bd9ed51133d598297
Yedil·Serzhan <yedilserzhan@gmail.com>
2022-07-25 00:56:38 +0200

    add tpc run model

--- rest_api/tpch/models.py
+++ rest_api/tpch/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Run(models.Model):
+    run_id = models.BigAutoField(primary_key=True)
+    date_submitted = models.DateTimeField(auto_now_add=True)
+    machine_id = models.ForeignKey('machines.Machine', on_delete=models.CASCADE, related_name='tpchrun')
+    scale_factor = models.IntegerField(null=True)
+    QphH = models.CharField(max_length=100)
