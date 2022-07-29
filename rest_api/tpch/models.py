4598b0e20466c7882a762f16ed04f2fb34f8f34e
Yedil·Serzhan <yedilserzhan@gmail.com>
2022-07-29 17:39:58 +0200

    add queryresult model for tpch

--- rest_api/tpch/models.py
+++ rest_api/tpch/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.core import validators
 
 
-# Create your models here.
+# model to store infos about each run of tpch
 class Run(models.Model):
-    run_id = models.BigAutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     date_submitted = models.DateTimeField()
     machine = models.ForeignKey('machines.Machine', on_delete=models.CASCADE, related_name='tpch_run')
     scale_factor = models.FloatField(validators=[validators.MinValueValidator(0)])
@@ -12,3 +12,11 @@ class Run(models.Model):
     throughput_score = models.FloatField(validators=[validators.MinValueValidator(0)])
     composite_score = models.FloatField(validators=[validators.MinValueValidator(0)])  # QphH score
 
+
+# model to store data for each query in the tpch query set executed during the run
+class QueryResult(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    query_idx = models.SmallIntegerField()
+    time = models.FloatField()
+    type = models.CharField(max_length=30)
+    run = models.ForeignKey('tpch.Run', on_delete=models.CASCADE, related_name='tpch_queryresult')
