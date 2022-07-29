94acbdf05c69d63ec97025644fbf38d5756a8f3f
Yedil·Serzhan <yedilserzhan@gmail.com>
2022-07-29 12:04:05 +0200

    add power and throughput score fields to tpch model

--- rest_api/tpch/models.py
+++ rest_api/tpch/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.core import validators
 
+
 # Create your models here.
 class Run(models.Model):
     run_id = models.BigAutoField(primary_key=True)
     date_submitted = models.DateTimeField()
     machine = models.ForeignKey('machines.Machine', on_delete=models.CASCADE, related_name='tpch_run')
     scale_factor = models.FloatField(validators=[validators.MinValueValidator(0)])
-    qphh = models.FloatField(validators=[validators.MinValueValidator(0)])
+    power_score = models.FloatField(validators=[validators.MinValueValidator(0)])
+    throughput_score = models.FloatField(validators=[validators.MinValueValidator(0)])
+    composite_score = models.FloatField(validators=[validators.MinValueValidator(0)])  # QphH score
+
