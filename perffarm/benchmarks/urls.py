880d225adffbbea468cd0da0763c37241836881d
lord2anil <a8208226@gmail.com>
2023-08-07 10:23:42 +0530

    adding url for  pgbench_custom

--- perffarm/benchmarks/urls.py
+++ perffarm/benchmarks/urls.py
@@ -6,10 +6,10 @@ app_name = "benchmarks"
 
 urlpatterns = [
     url('overview', views.overview_view),
-    url('machines', views.pgbench_benchmark_machines_view, name='benchmarks_machines'),
+    url(r'^machines/(?P<type>\w+)/$', views.pgbench_benchmark_machines_view, name='benchmarks_machines'),
     url(r'history/(?P<machine>.+)/', views.machine_history_view, name='machine_history'),
     url(r'postgres/(?P<machine>.+)/', views.postgres_history_view, name='postgres-history'),
-    url(r'pgbench_trends/(?P<machine>.+)/(?P<config>.+)/', views.pgbench_benchmark_trend_view, name='pgbench_trends-detail'),
-    url(r'pgbench_results_commit/(?P<commit>.+)/(?P<machine>.+)/(?P<config>.+)/', views.pgbench_runs_view, name='pgbench_trends-commit'),
+    url(r'pgbench_trends/(?P<type>\w+)/(?P<machine>.+)/(?P<config>.+)/', views.pgbench_benchmark_trend_view, name='pgbench_trends-detail'),
+    url(r'pgbench_results_commit/(?P<type>\w+)/(?P<commit>.+)/(?P<machine>.+)/(?P<config>.+)/', views.pgbench_runs_view, name='pgbench_trends-commit'),
     url(r'pgbench_results_complete/(?P<id>.+)/', views.pgbench_result_complete_view),
 ]
